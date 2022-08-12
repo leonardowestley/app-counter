d2c6b2a8448254456602ddcf37939e24299006cd
Leonaro Todd Westley <ltwestley6@gmail.com>
2022-08-12 17:37:10 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// initialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+  count += 1
+  countEl.innerText = count
+}
+
+function save() {
+  console.log(count)
+}
+save()
