e14a973c58e4f0b031c66b3204a9d9222bfac9c7
Leonaro Todd Westley <ltwestley6@gmail.com>
2022-08-17 09:13:19 -0400

    second commit

--- index.js
+++ index.js
@@ -3,15 +3,27 @@
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
 
-let countEl = document.getElementById("count-el")
-let count = 0
+// let countEl = document.getElementById("count-el")
+// let count = 0
 
-function increment() {
-  count += 1
-  countEl.innerText = count
-}
+// function increment() {
+//   count += 1
+//   countEl.innerText = count
+// }
 
-function save() {
-  console.log(count)
-}
-save()
+// function save() {
+//   console.log(count)
+// }
+// save()
+
+let welcomeEl =
+  document.getElementById(
+    "welcome-el"
+  );
+let myName =
+  "Todd";
+let greeting =
+  "Hi, my name is ";
+welcomeEl.innerText =
+  greeting +
+  myName;
