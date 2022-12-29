4c845d17fb2ee99f2c3ad458a496b1d2629bff96
Leonaro Todd Westley <ltwestley6@gmail.com>
2022-12-29 15:57:21 -0500

    update2

--- index.js
+++ index.js
@@ -3,27 +3,14 @@
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
 
-// let countEl = document.getElementById("count-el")
-// let count = 0
+let countEl = document.getElementById("count-el");
+let count = 0;
 
-// function increment() {
-//   count += 1
-//   countEl.innerText = count
-// }
+function increment() {
+  count += 1;
+  countEl.innerText = count;
+}
 
-// function save() {
-//   console.log(count)
-// }
-// save()
-
-let welcomeEl =
-  document.getElementById(
-    "welcome-el"
-  );
-let myName =
-  "Todd";
-let greeting =
-  "Hi, my name is ";
-welcomeEl.innerText =
-  greeting +
-  myName;
+function save() {
+  console.log(count);
+}
